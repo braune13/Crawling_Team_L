882fb1293f8c7b918db293697183b5a9e8b09c38
Justin Mahoney <justinmahoneycs@gmail.com>
2017-12-06 20:34:58 -0500

    Fixed Indentation Issue

--- Crawling_L.py
+++ Crawling_L.py
@@ -38,48 +38,48 @@ def parse_webpages(webpages):
             directory=page.rsplit("/",1)[0]
             link=directory+'/'+file
 
-        if file.endswith(('txt','md')): #can be expanded to other file types with a comma
-            text=bs4.BeautifulSoup(requests.get(link).text, "html.parser")
-            ext=link.rsplit(".",1)[-1]
-            text=[link,ext,"text:",text]
-            docs.append(text)
-        elif file.endswith(('pdf')): # special case if PDF
-            pdf=file.rsplit("/",1)[-1]
-            try:
-                response = urlopen(link) # must first check if pdf is found
-
-            except urllib.error.HTTPError as e:
-                text=[link,"pdf","text:","404"] #if 404 error, put 404 as text
+            if file.endswith(('txt','md')): #can be expanded to other file types with a comma
+                text=bs4.BeautifulSoup(requests.get(link).text, "html.parser")
+                ext=link.rsplit(".",1)[-1]
+                text=[link,ext,"text:",text]
                 docs.append(text)
+            elif file.endswith(('pdf')): # special case if PDF
+                pdf=file.rsplit("/",1)[-1]
+                try:
+                    response = urlopen(link) # must first check if pdf is found
 
-            else:
-                file = open(pdf, 'wb') #otherwise must save the pdf to run pypdf2
-                file.write(response.read())
-                file.close()
+                except urllib.error.HTTPError as e:
+                    text=[link,"pdf","text:","404"] #if 404 error, put 404 as text
+                    docs.append(text)
 
-                txt=""
-                file=open(pdf,'rb')
-                parser = PDFParser(file)
-                document = PDFDocument(parser)
-                rsrcmgr = PDFResourceManager()
-                laparams = LAParams()
-                device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-                interpreter = PDFPageInterpreter(rsrcmgr, device)
-                for p in PDFPage.create_pages(document):
-                	# As the interpreter processes the page stored in PDFDocument object
-                	interpreter.process_page(p)
-                	# The device renders the layout from interpreter
-                	layout = device.get_result()
-                	# Out of the many LT objects within layout, we are interested in LTTextBox and LTTextLine
-                	for lt_obj in layout:
-                		if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-                			txt += lt_obj.get_text()
+                else:
+                    file = open(pdf, 'wb') #otherwise must save the pdf to run pypdf2
+                    file.write(response.read())
+                    file.close()
 
-                #close the pdf file
-                file.close()
-                name=[link,"pdf","text:", txt]
-                os.remove(pdf) #remove the saved file when done
-                docs.append(name)
+                    txt=""
+                    file=open(pdf,'rb')
+                    parser = PDFParser(file)
+                    document = PDFDocument(parser)
+                    rsrcmgr = PDFResourceManager()
+                    laparams = LAParams()
+                    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+                    interpreter = PDFPageInterpreter(rsrcmgr, device)
+                    for p in PDFPage.create_pages(document):
+                        # As the interpreter processes the page stored in PDFDocument object
+                        interpreter.process_page(p)
+                        # The device renders the layout from interpreter
+                        layout = device.get_result()
+                        # Out of the many LT objects within layout, we are interested in LTTextBox and LTTextLine
+                        for lt_obj in layout:
+                            if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                                txt += lt_obj.get_text()
+
+                    #close the pdf file
+                    file.close()
+                    name=[link,"pdf","text:", txt]
+                    os.remove(pdf) #remove the saved file when done
+                    docs.append(name)
 
         docs=[[str(i) for i in lis] for lis in docs]
         timestamp = datetime.datetime.now().isoformat()
